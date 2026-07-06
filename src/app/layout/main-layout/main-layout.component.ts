82c1655344399ed378bc2302174156a598c5c16a
Abdallah Amin <abdoamen040466@gmail.com>
2026-07-06 03:46:06 +0300

    feat: create project architecture

---
+++ src/app/layout/main-layout/main-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-main-layout',
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  templateUrl: './main-layout.component.html',
+  styleUrl: './main-layout.component.css',
+})
+export class MainLayoutComponent {}
