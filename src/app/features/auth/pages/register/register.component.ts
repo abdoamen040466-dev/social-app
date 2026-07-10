2b2dbf3ef6fca95949a155aef11dc4018d35aeae
Abdallah Amin <abdoamen040466@gmail.com>
2026-07-10 20:43:37 +0300

    feat: implement register reactive form

--- src/app/features/auth/pages/register/register.component.ts
+++ src/app/features/auth/pages/register/register.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  readonly registerForm = new FormGroup({
+    name: new FormControl(''),
+    username: new FormControl(''),
+    email: new FormControl(''),
+    dateOfBirth: new FormControl(''),
+    gender: new FormControl(''),
+    password: new FormControl(''),
+    rePassword: new FormControl(''),
+  });
+
+  submitForm(): void {
+    console.log(this.registerForm.value);
+  }
+}
