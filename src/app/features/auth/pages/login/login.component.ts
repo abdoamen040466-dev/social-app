aa3c43a6069cad517e0188a137923162c18e7500
Abdallah Amin <abdoamen040466@gmail.com>
2026-07-14 00:20:29 +0300

    feat(auth): implement login form

--- src/app/features/auth/pages/login/login.component.ts
+++ src/app/features/auth/pages/login/login.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  readonly loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required),
+  });
+
+  get input() {
+    return this.loginForm.controls;
+  }
+
+  submitLogin() {
+    console.log(this.loginForm);
+  }
+}
