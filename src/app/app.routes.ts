c3b45001a98711bd2cb820c4cb9ab62576960ecb
Abdallah Amin <abdoamen040466@gmail.com>
2026-08-03 00:44:07 +0300

    feat(auth): implement route guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,24 +7,36 @@ import { HomeComponent } from './features/home/pages/home/home.component';
 import { ProfileComponent } from './features/profile/pages/profile/profile.component';
 import { NotificationsComponent } from './features/notifications/pages/notifications/notifications.component';
 import { NotFoundComponent } from './features/not-found/pages/not-found/not-found.component';
+import { authGuard } from './core/auth/guards/auth-guard-guard';
+import { guestGuard } from './core/auth/guards/guest-guard';
 
 export const routes: Routes = [
   {
     path: '',
     component: AuthLayoutComponent,
     children: [
-      { path: 'register', component: RegisterComponent, title: 'register' },
+      {
+        path: 'register',
+        component: RegisterComponent,
+        title: 'register',
+        canActivate: [guestGuard],
+      },
       { path: '', redirectTo: 'register', pathMatch: 'full', title: 'home' },
-      { path: 'login', component: LoginComponent, title: 'login' },
+      { path: 'login', component: LoginComponent, title: 'login', canActivate: [guestGuard] },
     ],
   },
   {
     path: '',
     component: MainLayoutComponent,
     children: [
-      { path: 'home', component: HomeComponent, title: 'home' },
-      { path: 'profile', component: ProfileComponent, title: 'profile' },
-      { path: 'notifications', component: NotificationsComponent, title: 'notifications' },
+      { path: 'home', component: HomeComponent, title: 'home', canActivate: [authGuard] },
+      { path: 'profile', component: ProfileComponent, title: 'profile', canActivate: [authGuard] },
+      {
+        path: 'notifications',
+        component: NotificationsComponent,
+        title: 'notifications',
+        canActivate: [authGuard],
+      },
       { path: '**', component: NotFoundComponent, title: 'notFound' },
     ],
   },
